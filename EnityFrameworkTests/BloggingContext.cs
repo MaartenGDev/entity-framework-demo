abbe95eb55f78a7f3f36b5dc4fdf90ad9cf03dbb
MaartenGDev <maarten.grootoonk@gmail.com>
2022-02-14 11:40:29 +0100

    Initial commit

---
+++ EnityFrameworkTests/BloggingContext.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace EnityFrameworkTests;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            @"Server=127.0.0.1;Database=EntityFrameworkTests;Uid=sa;Pwd=yourStrong(!)Password;");
+    }
+}
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string Url { get; set; }
+    public int Rating { get; set; }
+    public List<Post> Posts { get; set; }
+}
+
+public class Post
+{
+    public int PostId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public int BlogId { get; set; }
+    public Blog Blog { get; set; }
+}
---
+++ EnityFrameworkTests/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EnityFrameworkTests;
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, World!");
+//
+// using (var db = new BloggingContext())
+// {
+//     var blog = new Blog { Url = "ello" };
+//     db.Blogs.Add(blog);
+//     db.SaveChanges();
+// }
+
+using (var db = new BloggingContext())
+{
+    var blogs = db.Blogs;
+
+    foreach (var blog in blogs)
+    {
+        Console.WriteLine($"Found: {blog.Url}");
+    }
+}
